f06dbf272927dd8085bfda611de33f1151550ec8
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2026-03-19 14:50:21 -0300

    insert, deletes  test City Controller

--- src/main/java/com/devsuperior/bds02/service/CityService.java
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -3,9 +3,13 @@ package com.devsuperior.bds02.service;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.service.exceptions.DatabaseException;
+import com.devsuperior.bds02.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -23,4 +27,25 @@ public class CityService {
         return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City city = new City();
+        city.setName(dto.getName());
+        city = repository.save(city);
+
+        return new CityDTO(city);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
 }
